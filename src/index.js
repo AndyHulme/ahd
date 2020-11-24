07109d253c68a63e7f8422267339271a894169a8
Andy Hulme <andyhulme@Andys-MacBook-Pro.local>
2020-11-24 12:27:15 +0000

    init +

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@
 
 import "./css/main.css";
 
-// Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+//lightmode
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light'); //add this
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark'); //add this
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+
+
